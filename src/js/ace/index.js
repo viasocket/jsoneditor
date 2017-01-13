76ae4c7b7aff6dcf00a8cc562335c609f0d0aa87
sameer <sameer@walkover.in>
2017-01-13 16:29:46 +0530

    fix

--- src/js/ace/index.js
+++ src/js/ace/index.js
@@ -4,5 +4,6 @@ var ace = require('brace');
 // load required ace modules
 require('brace/mode/json');
 require('brace/ext/searchbox');
+require('brace/ext/language_tools.js');
 
 module.exports = ace;
