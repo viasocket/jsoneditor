e027757226faca18f799fe5199d95b937c176a8f
Sarfaraz Ansari <sarfaraz@walkover.in>
2017-01-14 12:35:27 +0530

    fix pulls

--- dist/jsoneditor.js
+++ dist/jsoneditor.js
@@ -25,7 +25,7 @@
  *
  * @author  Jos de Jong, <wjosdejong@gmail.com>
  * @version 5.5.11
- * @date    2017-01-13
+ * @date    2017-01-14
  */
 (function webpackUniversalModuleDefinition(root, factory) {
 	if(typeof exports === 'object' && typeof module === 'object')
